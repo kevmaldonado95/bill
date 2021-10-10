430747145f97b9aae8932303fd3aefb094bf5ae7
Kevin Maldonado <kevin.maldonado95@outlook.com>
2021-10-10 13:57:50 -0600

    sexto

--- bill.js
+++ bill.js
@@ -1,10 +1,10 @@
 
 function calculateBill(){
     let montoIndividual;
-    let isPercent = document.getElementById("isPercent").value;
-    let monto = document.getElementById("monto");
-    let propina = document.getElementById("propina");
-    let personas = document.getElementById("personas");
+    let isPercent = parseInt(document.getElementById("isPercent").value);
+    let monto = parseInt(document.getElementById("monto"));
+    let propina = parseInt(document.getElementById("propina"));
+    let personas = parseInt(document.getElementById("personas"));
     if(isPercent){
         montoIndividual = monto*(1+propina/100)/personas;
     }else{
